6f3b70a655a23476edd62e88e1feafbeed3c40d9
Valen-0612 <vgr230115@gmail.com>
2025-05-19 20:12:02 +0000

    landing page valentina gil

---
+++ src/js/components/JumboTron.jsx
@@ -0,0 +1,32 @@
+
+// 1.- 
+
+
+// 5 y 2 
+// JumboTron.jsx
+export const JumboTron = () => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-sm-12 col-md-12 col-lg-12">
+          <div className="p-5 mb-4 bg-light rounded-3 mt-2">
+            <div className="container-fluid py-5">
+              <h1 className="display-5 fw-bold">Descubre el sonido del vinilo!</h1>
+              <p className="col-md-8 fs-4">
+                Bienvenidos a nuestra tienda de vinilos clasicos y contemporaneos
+              </p>
+              <button className="btn btn-primary btn-lg" type="button">
+                ver catalogo
+              </button>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+
+  );
+};
+
+
